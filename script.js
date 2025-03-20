38d0a7d7cf8166317a83e370d1366b068089aa69
okour05 <mr.hopez90@gmail.com>
2025-03-20 16:38:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    const menuList = document.querySelector('.menuList');
+    if (menuList.style.display === 'block') {
+        menuList.style.display = 'none';
+    } else {
+        menuList.style.display = 'block';
+    }
+}
